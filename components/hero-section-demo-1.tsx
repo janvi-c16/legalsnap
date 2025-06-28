32dbcb3b30448f047eda4f4ee65f33af569eab08
janvi-c16 <janvichauhan1608@gmail.com>
2025-06-28 16:29:21 +0530

    First commit

--- components/hero-section-demo-1.tsx
+++ components/hero-section-demo-1.tsx
@@ -2,11 +2,10 @@
 
 
 import { motion } from "motion/react";
-import FeatureBentoGrid from "./_componets/FeatureBentoGrid";
 
-export default function Home() {
+export default function HeroSectionOne() {
   return (
-    <div className="relative mx-auto my-10 flex flex-col items-center justify-center">
+    <div className="relative mx-auto my-10 flex max-w-7xl flex-col items-center justify-center">
       <Navbar />
       <div className="absolute inset-y-0 left-0 h-full w-px bg-neutral-200/80 dark:bg-neutral-800/80">
         <div className="absolute top-0 h-40 w-px bg-gradient-to-b from-transparent via-blue-500 to-transparent" />
@@ -98,7 +97,6 @@ export default function Home() {
               width={1000}
             />
           </div>
-          <FeatureBentoGrid />
         </motion.div>
       </div>
     </div>
@@ -110,7 +108,7 @@ const Navbar = () => {
     <nav className="flex w-full items-center justify-between border-t border-b border-neutral-200 px-4 py-4 dark:border-neutral-800">
       <div className="flex items-center gap-2">
         <div className="size-7 rounded-full bg-gradient-to-br from-violet-500 to-pink-500" />
-        <h1 className="text-base font-bold md:text-2xl">LegalSnap</h1>
+        <h1 className="text-base font-bold md:text-2xl">Aceternity UI</h1>
       </div>
       <button className="w-24 transform rounded-lg bg-black px-6 py-2 font-medium text-white transition-all duration-300 hover:-translate-y-0.5 hover:bg-gray-800 md:w-32 dark:bg-white dark:text-black dark:hover:bg-gray-200">
         Login
